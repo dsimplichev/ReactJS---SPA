a73f235e8004f0c7eea7b7634f9872ec13f313ff
dsimplichev <dsimplichev1948@gmail.com>
2024-04-29 15:49:09 +0100

    add second components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./components/Home";
+import Products from "./components/Products";
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
+  { path: '/products', element: <Products /> }
   
 ])
 
