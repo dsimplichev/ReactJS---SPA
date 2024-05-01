1d54e001072026e7c54d83bf1162ca0fa70adec8
dsimplichev <dsimplichev1948@gmail.com>
2024-05-01 14:21:16 +0100

    add Error page

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../components/Navigation";
-import classes from './Root.module.css'
+
 
 function RootLayout() {
 return (
     <>
     <Navigation />
-    <main className="{classes.content}">
+    <main>
     <Outlet />
     </main>
     </>
