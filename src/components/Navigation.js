975b61c7bf245e9ebc000016c7ff431260b4e5f6
dsimplichev <dsimplichev1948@gmail.com>
2024-05-01 14:13:50 +0100

    add css for root and navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-
+import classes from './Navigation.module.css'
 
 function Navigation() {
     return (
-        <header>
+        <header className={classes.header}>
             <nav>
-                <ul>
+                <ul className={classes.list}>
                     <li>
                         <Link to="/">Home</Link>
                     </li>
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../components/Navigation";
-
+import classes from './Root.module.css'
 
 function RootLayout() {
 return (
     <>
     <Navigation />
+    <main className="{classes.content}">
     <Outlet />
+    </main>
     </>
 )
 }
